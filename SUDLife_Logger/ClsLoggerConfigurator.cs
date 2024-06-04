0bd16b1a4c3152fb3dbb3eefc9e1010a8e57f007
ShubhamMadhukarSUD <shubham.madhukar@sudlife.in>
2024-06-04 12:01:55 +0530

    Implemented JWT Authentication and SeriLog

---
+++ SUDLife_Logger/ClsLoggerConfigurator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+
+
+namespace SUDLife_Logger
+{
+    public static class ClsLoggerConfigurator
+    {
+        public static void ConfigureLogger(IConfiguration configuration)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        public static void CloseLogger()
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
