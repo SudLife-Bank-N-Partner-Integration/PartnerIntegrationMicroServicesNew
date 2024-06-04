5d509c5c62238c8f9d31e0000292b953c3247b7d
ShubhamMadhukarSUD <shubham.madhukar@sudlife.in>
2024-06-04 12:46:17 +0530

    Revised Serilog Logic

--- SUDLife_Aadarsh/Controllers/AadarshController.cs
+++ SUDLife_Aadarsh/Controllers/AadarshController.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using RestSharp;
+using SUDLife_Aadarsh.Datalayer;
 using SUDLife_Aadarsh.Models.Request;
 using SUDLife_Aadarsh.Models.Response;
 using SUDLife_Aadarsh.ServiceLayer;
 using SUDLife_CallThirdPartyAPI;
+using System;
+using System.Data;
+using System.Threading.Tasks;
 
 namespace SUDLife_Aadarsh.Controllers
 {
@@ -13,79 +19,95 @@ namespace SUDLife_Aadarsh.Controllers
     [ApiController]
     public class AadarshController : ControllerBase
     {
+        private readonly ILogger<AadarshController> _logger;
         private IConfiguration config { get; set; }
         private readonly ThirdPartyAPI _thirdPartyAPI;
         private readonly ClsAadarsh _clsAadarsh;
 
-        public AadarshController(IConfiguration configuration, ThirdPartyAPI thirdPartyAPI, ClsAadarsh clsAadarsh)
+        public AadarshController(
+            IConfiguration configuration,
+            ThirdPartyAPI thirdPartyAPI,
+            ClsAadarsh clsAadarsh,
+            ILogger<AadarshController> logger)
         {
             this.config = configuration;
             this._thirdPartyAPI = thirdPartyAPI;
             this._clsAadarsh = clsAadarsh;
+            this._logger = logger;
         }
 
         [HttpPost("Aadarsh")]
         public async Task<IActionResult> CallThirdParty([FromBody] ClsAadarshPlainRequest request)
         {
-            ClsSDEBasePremiumRequest root = await _clsAadarsh.AadarshDetails(request);
+            _logger.LogInformation("Received request in CallThirdParty action");
 
-            ClsAadarshPlainResponse _AadarshResponse = new ClsAadarshPlainResponse();
-            // Third party API URL
-            string url = "https://siapi.sudlife.in/nsureservices.svc/generatebiapi";
-
-            // Serialize request to JSON
-            string jsonBody = System.Text.Json.JsonSerializer.Serialize(root);
-            var APIRequest = jsonBody.Replace("null", "[]");
-
-            // Calling the third-party API
-            dynamic ResponseFromNsureservice = _thirdPartyAPI.ClientAPI(url, Method.Post, jsonBody);
-
-            // Map the response to your response model
-            if (!string.IsNullOrEmpty(ResponseFromNsureservice))
+            try
             {
-                var jsonString2 = JsonConvert.DeserializeObject<ClsSDEBasePremiumResponse>(ResponseFromNsureservice);
-                if (jsonString2.Status == "Fail" || jsonString2.BIJson == null)
-                {
-                    _AadarshResponse.Status = jsonString2.Status;
-                    _AadarshResponse.Message = jsonString2.Message;
-                    _AadarshResponse.TransactionId = jsonString2.TransactionId;
+                //UpdateLog();
 
-                    if (jsonString2.InputValidationStatus != null)
+                ClsSDEBasePremiumRequest root = await _clsAadarsh.AadarshDetails(request);
+                ClsAadarshPlainResponse _AadarshResponse = new ClsAadarshPlainResponse();
+
+                string url = "https://siapi.sudlife.in/nsureservices.svc/generatebiapi";
+
+                string jsonBody = System.Text.Json.JsonSerializer.Serialize(root);
+                var APIRequest = jsonBody.Replace("null", "[]");
+
+                dynamic ResponseFromNsureservice = _thirdPartyAPI.ClientAPI(url, Method.Post, jsonBody);
+
+                if (!string.IsNullOrEmpty(ResponseFromNsureservice))
+                {
+                    var jsonString2 = JsonConvert.DeserializeObject<ClsSDEBasePremiumResponse>(ResponseFromNsureservice);
+                    if (jsonString2.Status == "Fail" || jsonString2.BIJson == null)
                     {
-                        if (jsonString2.InputValidationStatus[0].IpKwMessage.Count > 0)
+                        _AadarshResponse.Status = jsonString2.Status;
+                        _AadarshResponse.Message = jsonString2.Message;
+                        _AadarshResponse.TransactionId = jsonString2.TransactionId;
+
+                        if (jsonString2.InputValidationStatus != null)
                         {
-                            _AadarshResponse.Message += Environment.NewLine + jsonString2.InputValidationStatus[0].IpKwMessage[0];
-                        }
-                        else
-                        {
-                            _AadarshResponse.Message += Environment.NewLine + jsonString2.InputValidationStatus[0].GeneralError;
+                            if (jsonString2.InputValidationStatus[0].IpKwMessage.Count > 0)
+                            {
+                                _AadarshResponse.Message += Environment.NewLine + jsonString2.InputValidationStatus[0].IpKwMessage[0];
+                            }
+                            else
+                            {
+                                _AadarshResponse.Message += Environment.NewLine + jsonString2.InputValidationStatus[0].GeneralError;
+                            }
                         }
                     }
+                    else
+                    {
+                        var InputValidationStatus = jsonString2.InputValidationStatus;
+                        _AadarshResponse.ModalPremium = Math.Round(InputValidationStatus[0].ModalPremium);
+                        _AadarshResponse.Tax = Math.Round(InputValidationStatus[0].Tax);
+                        _AadarshResponse.ModalPremiumwithTax = Math.Round((_AadarshResponse.ModalPremium + _AadarshResponse.Tax));
 
+                        _AadarshResponse.Message = jsonString2.Message;
+                        _AadarshResponse.Status = jsonString2.Status;
+                        _AadarshResponse.TransactionId = jsonString2.TransactionId;
+                    }
                 }
                 else
                 {
-                    var InputValidationStatus = jsonString2.InputValidationStatus;
-                    _AadarshResponse.ModalPremium = Math.Round(InputValidationStatus[0].ModalPremium);
-                    _AadarshResponse.Tax = Math.Round(InputValidationStatus[0].Tax);
-                    _AadarshResponse.ModalPremiumwithTax = Math.Round((_AadarshResponse.ModalPremium + _AadarshResponse.Tax));
-
-                    _AadarshResponse.Message = jsonString2.Message;
-                    _AadarshResponse.Status = jsonString2.Status;
-                    _AadarshResponse.TransactionId = jsonString2.TransactionId;
-
-
+                    _AadarshResponse.Status = "Failure - Service Calling";
+                    _AadarshResponse.Message = "Error occured while consuming SDE API.Please contact SUD Admin";
                 }
 
+                return Ok(_AadarshResponse);
             }
-            else
+            catch (Exception ex)
             {
-                //fail response
-                _AadarshResponse.Status = "Failure - Service Calling";
-                _AadarshResponse.Message = "Error occured while consuming SDE API.Please contact SUD Admin";
+                _logger.LogError(ex, "An error occurred in CallThirdParty action");
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred");
             }
+        }
 
-            return Ok(_AadarshResponse);
+        public void UpdateLog()
+        {
+            UpdateLogs logs = new UpdateLogs();
+            logs.ExecuteProc();
+            _logger.LogInformation("Updated logs successfully");
         }
     }
 }
--- SUDLife_Aadarsh/Program.cs
+++ SUDLife_Aadarsh/Program.cs
@@ -1,20 +1,58 @@
-﻿using SUDLife_Aadarsh.Controllers;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
 using SUDLife_Aadarsh.Models.Request;
 using SUDLife_Aadarsh.ServiceLayer;
 using SUDLife_CallThirdPartyAPI;
+using SUDLife_Logger; 
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Loading configuration
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+    .Build();
+
+// Configure\ing Serilog
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+// Using Serilog for logging
+builder.Host.UseSerilog();
+
 // Add services to the container.
-
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ClsCommonOperations>();
 builder.Services.AddScoped<ThirdPartyAPI>();
 builder.Services.AddScoped<ClsAadarsh>();
 builder.Services.AddScoped<ClsSDEBasePremiumRequest>();
+
+string issuer = "CCDD18D8-49FF-43E5-8E6C-D0924C2BBE0C";
+string audience = "B502C4CA-9895-419A-AF6C-65B5801CBDEA";
+string symmetricSecurityKey = "463C24EE-EE45-4957-A682-3704AD7F91C7";
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(symmetricSecurityKey))
+    };
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -22,14 +60,24 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-};
+}
 
 // Configure the HTTP request pipeline.
-
+app.UseAuthentication();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
-app.Run();
+try
+{
+    Log.Information("Starting web host");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    ClsLoggerConfigurator.CloseLogger();
+}
